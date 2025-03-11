e48f2da0cde1bbbfa816224724932ab5a22130a7
Gojimandev <99045356+Gojimandev@users.noreply.github.com>
2025-03-11 11:34:26 +0100

    test parsing for the XML preview

--- src/js/editor-parser.js
+++ src/js/editor-parser.js
@@ -1,6 +1,39 @@
+document.getElementById('fileinput').addEventListener('change', handleFileSelect, false);
+
 var editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
     mode: "xml",
     theme: "dracula", // Change theme if needed
     lineNumbers: true,
     autoCloseTags: true
 });
+
+function handleFileSelect(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            const parser = new DOMParser();
+            const xmlDoc = parser.parseFromString(e.target.result, "text/xml");
+            populateXMLPreview(xmlDoc);
+        };
+        reader.readAsText(file);
+    }
+}
+function populateXMLPreview(xmlDoc) {
+    console.log(xmlDoc);
+    console.log(editor);
+    console.log(editor.getTextArea());
+    const  XMLcontainer = editor.getTextArea();
+    XMLcontainer.innerHTML = ''; // Clear existing content
+    let xmlContent = "";
+    const elements = xmlDoc.getElementsByTagName('*');
+    console.log(elements); 
+    for (let i = 0; i < elements.length; i++) {
+        const element = elements[i];
+        xmlContent += element.outerHTML;
+    }
+    editor.setValue(xmlContent);
+}
+function addToEditor(xmlObj){
+    editor.setValue(editor.getValue() + xmlObj.outerHTML);
+}
